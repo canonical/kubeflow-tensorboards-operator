201a0c6a67b840882f4198668586a771336e537b
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2021-11-22 13:43:16 +0100

    Fix Status Handling and License Headers
    - Fix Status Handling by always calling main and adding checks
    - Add license headers and checks
    - Fix charmcraft parts issue

--- operators/tensorboards-web-app/tests/unit/test_charm.py
+++ operators/tensorboards-web-app/tests/unit/test_charm.py
@@ -1,3 +1,6 @@
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 import pytest
 
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
@@ -13,7 +16,7 @@ def harness():
 
 
 def test_not_leader(harness):
-    harness.begin()
+    harness.begin_with_initial_hooks()
     assert isinstance(harness.charm.model.unit.status, WaitingStatus)
 
 
