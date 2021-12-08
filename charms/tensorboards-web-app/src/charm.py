1e75c774f50d4018b62ea7b2dec7893f439aa3f2
Andrew Scribner <ca.scribner+1@gmail.com>
2021-12-08 09:51:10 -0500

    fix: force CheckFailed msg arg to string
    fixes error where non-string input will cause an exception when instantiating the `status`

--- charms/tensorboards-web-app/src/charm.py
+++ charms/tensorboards-web-app/src/charm.py
@@ -18,12 +18,12 @@ from serialized_data_interface import (
 class CheckFailed(Exception):
     """ Raise this exception if one of the checks in main fails. """
 
-    def __init__(self, msg, status_type=None):
+    def __init__(self, msg: str, status_type=None):
         super().__init__()
 
-        self.msg = msg
+        self.msg = str(msg)
         self.status_type = status_type
-        self.status = status_type(msg)
+        self.status = status_type(self.msg)
 
 
 class Operator(CharmBase):
