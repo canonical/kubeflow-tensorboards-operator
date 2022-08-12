0a775dbea270b3d70195e07798a72cf06f6274a7
ca-scribner <ca.scribner+1@gmail.com>
2022-08-12 20:52:28 +0000

    Apply commit-cannon changes

--- .github/workflows/get-charm-paths.sh
+++ .github/workflows/get-charm-paths.sh
@@ -10,22 +10,21 @@
 CHARMS_DIR="./charms"
 if [ -d "$CHARMS_DIR" ];
 then
-	CHARM_PATHS=$(find $CHARMS_DIR -maxdepth 1 -type d -not -path '*/\.*' -not -path "$CHARMS_DIR")
+  CHARM_PATHS=$(find $CHARMS_DIR -maxdepth 1 -type d -not -path '*/\.*' -not -path "$CHARMS_DIR")
 else
-	if [ -f "./metadata.yaml" ]
-	then
-		CHARM_PATHS="./"
-	else
-		echo "Cannot find valid charm directories - aborting"
-		exit 1
-	fi
+  if [ -f "./metadata.yaml" ]
+  then
+    CHARM_PATHS="./"
+  else
+    echo "Cannot find valid charm directories - aborting"
+    exit 1
+  fi
 fi
 
-# Convert to JSON in format:
+# Convert output to JSON string format
 # { charm_paths: [...] }
-CHARM_PATHS_JSON=$(echo "$CHARM_PATHS" | jq --slurp --raw-input 'split("\n")[:-1]' | jq "{\"charm-path\": . }" | jq -c)
+CHARM_PATHS_LIST=$(echo "$CHARM_PATHS" | jq -c --slurp --raw-input 'split("\n")[:-1]')
 
-echo "Found CHARM_PATHS_JSON: $CHARM_PATHS_JSON"
+echo "Found CHARM_PATHS_LIST: $CHARM_PATHS_LIST"
 
-echo "::set-output name=CHARM_PATHS_JSON::$CHARM_PATHS_JSON"
-
+echo "::set-output name=CHARM_PATHS_LIST::$CHARM_PATHS_LIST"
