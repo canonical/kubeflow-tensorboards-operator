3689ff94edac1952f47abc70e2b5c02ed3f4e8e2
natalia <natalianowakowska@mail.com>
2022-08-17 14:52:19 +0200

    remove duplicated test_no_relation

--- charms/tensorboards-web-app/tests/unit/test_charm.py
+++ charms/tensorboards-web-app/tests/unit/test_charm.py
@@ -30,21 +30,6 @@ def test_missing_relation(harness):
     missing_relation(harness, ActiveStatus)
 
 
-def test_no_relation(harness):
-    harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
-    harness.begin_with_initial_hooks()
-
-    assert harness.charm.model.unit.status == ActiveStatus()
-
-
 def test_main(harness):
     harness.set_leader(True)
     harness.add_oci_resource(
