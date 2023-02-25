28ea2cf47e1ae09e1c9350f08f90cef6a62d4a2c
sed-i <82407168+sed-i@users.noreply.github.com>
2023-02-24 23:41:19 -0500

    Reorg itests

---
+++ charms/tensorboard-controller/tests/integration/test_charm.py
@@ -0,0 +1,49 @@
+import asyncio
+import logging
+from pathlib import Path
+from types import SimpleNamespace
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+log = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
+tc = SimpleNamespace(name="tensorboard-controller", resources=resources)
+
+istio_gw = SimpleNamespace(charm="ch:istio-gateway", name="istio-ingressgateway", channel="latest/edge", config={"kind": "ingress"})
+istio_pilot = SimpleNamespace(charm="ch:istio-pilot", name="istio-pilot", channel="latest/edge", config={"default-gateway": "test-gateway"})
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy_with_relations(ops_test: OpsTest, tc_charm):
+    await asyncio.gather(
+        ops_test.model.deploy(
+            tc_charm,
+            resources=tc.resources,
+            application_name=tc.name
+        ),
+        ops_test.model.deploy(
+            istio_gw.charm,
+            application_name=istio_gw.name,
+            channel=istio_gw.channel,
+            config=istio_gw.config,
+            trust=True,
+        ),
+        ops_test.model.deploy(
+            istio_pilot.charm,
+            application_name=istio_pilot.name,
+            channel=istio_pilot.channel,
+            config=istio_pilot.config,
+            trust=True,
+        ),
+    )
+
+    await asyncio.gather(
+        ops_test.model.add_relation(istio_pilot.name, istio_gw.name),
+        ops_test.model.add_relation(f"{istio_pilot}:gateway-info", f"{tc.name}:gateway-info"),
+    )
+
+    await ops_test.model.wait_for_idle(timeout=60 * 10)
---
+++ charms/tensorboards-web-app/tests/integration/test_charms.py
@@ -0,0 +1,25 @@
+import logging
+from pathlib import Path
+from types import SimpleNamespace
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+log = logging.getLogger(__name__)
+
+TWA_METADATA = yaml.safe_load(Path("charms/tensorboards-web-app/metadata.yaml").read_text())
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
+twa = SimpleNamespace(name="tensorboard-webapp", resources=resources)
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest, twa_charm):
+    await ops_test.model.deploy(
+        twa_charm,
+        resources=twa.resources,
+        application_name=twa.name
+    )
+
+    await ops_test.model.wait_for_idle(timeout=60 * 10)
