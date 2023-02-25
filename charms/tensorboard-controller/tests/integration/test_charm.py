ec0f5208114ae35118840d882c028f18c340b069
sed-i <82407168+sed-i@users.noreply.github.com>
2023-02-24 23:44:59 -0500

    Lint

--- charms/tensorboard-controller/tests/integration/test_charm.py
+++ charms/tensorboard-controller/tests/integration/test_charm.py
@@ -13,18 +13,24 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 resources = {"oci-image": METADATA["resources"]["oci-image"]["upstream-source"]}
 tc = SimpleNamespace(name="tensorboard-controller", resources=resources)
 
-istio_gw = SimpleNamespace(charm="ch:istio-gateway", name="istio-ingressgateway", channel="latest/edge", config={"kind": "ingress"})
-istio_pilot = SimpleNamespace(charm="ch:istio-pilot", name="istio-pilot", channel="latest/edge", config={"default-gateway": "test-gateway"})
+istio_gw = SimpleNamespace(
+    charm="ch:istio-gateway",
+    name="istio-ingressgateway",
+    channel="latest/edge",
+    config={"kind": "ingress"},
+)
+istio_pilot = SimpleNamespace(
+    charm="ch:istio-pilot",
+    name="istio-pilot",
+    channel="latest/edge",
+    config={"default-gateway": "test-gateway"},
+)
 
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy_with_relations(ops_test: OpsTest, tc_charm):
     await asyncio.gather(
-        ops_test.model.deploy(
-            tc_charm,
-            resources=tc.resources,
-            application_name=tc.name
-        ),
+        ops_test.model.deploy(tc_charm, resources=tc.resources, application_name=tc.name),
         ops_test.model.deploy(
             istio_gw.charm,
             application_name=istio_gw.name,
