09c04c6096fd0626b8b44d4c917278e6ead42402
Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>
2023-05-29 14:34:19 +0300

    tests: Rewrite TB Controller unit tests
    Rewrite the Tensorboard Controller unit tests to account for the changes
    in the charm. More specifically:
    * Make sure that the simulation of container networking is enabled
    * Test that the charm waits if it's not the leader
    * Test that the charm waits if the gateway info relation is missing
    * Test that the charm goes to active status if the gateway info relation
      exists
    * Remove OCI images tests
    
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>

---
+++ charms/tensorboard-controller/tests/unit/test_operator.py
@@ -0,0 +1,72 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from unittest.mock import MagicMock, patch
+
+import pytest
+from ops.model import ActiveStatus, WaitingStatus
+from ops.testing import Harness
+
+from charm import TensorboardController
+
+
+@pytest.fixture(scope="function")
+def harness() -> Harness:
+    """Create and return Harness for testing."""
+    harness = Harness(TensorboardController)
+
+    # setup container networking simulation
+    harness.set_can_connect("tensorboard-controller", True)
+
+    return harness
+
+
+class TestCharm:
+    """Test class for Tensorboard Controller."""
+
+    def _setup_gateway_info_relation(
+        self, harness: Harness, model: str = "test-model", name: str = "test-gateway"
+    ):
+        """Setup the gateway info relation."""
+        dummy_relation = {"gateway_namespace": model, "gateway_name": name}
+        rel_id = harness.add_relation("gateway-info", "app")
+        harness.update_relation_data(rel_id, "app", dummy_relation)
+        harness.add_relation_unit(rel_id, "app/0")
+
+    @patch("charm.TensorboardController.k8s_resource_handler")
+    @patch("charm.TensorboardController.crd_resource_handler")
+    def test_not_leader(
+        self, k8s_resource_handler: MagicMock, crd_resource_handler: MagicMock, harness: Harness
+    ):
+        """Test that charm waits if it's not the leader."""
+        harness.begin_with_initial_hooks()
+
+        assert isinstance(harness.charm.model.unit.status, WaitingStatus)
+        assert harness.charm.model.unit.status == WaitingStatus("Waiting for leadership")
+
+    @patch("charm.TensorboardController.k8s_resource_handler")
+    @patch("charm.TensorboardController.crd_resource_handler")
+    def test_no_gateway_info_relation(
+        self, k8s_resource_handler: MagicMock, crd_resource_handler: MagicMock, harness: Harness
+    ):
+        """Test that charm waits if the gateway info relation is missing."""
+        harness.set_leader(True)
+        harness.begin_with_initial_hooks()
+
+        assert isinstance(harness.charm.model.unit.status, WaitingStatus)
+        assert harness.charm.model.unit.status == WaitingStatus(
+            "Waiting for gateway info relation"
+        )
+
+    @patch("charm.TensorboardController.k8s_resource_handler")
+    @patch("charm.TensorboardController.crd_resource_handler")
+    def test_active(
+        self, k8s_resource_handler: MagicMock, crd_resource_handler: MagicMock, harness: Harness
+    ):
+        """Test that charm goes to active status if the gateway info relation exists."""
+        harness.set_leader(True)
+        self._setup_gateway_info_relation(harness)
+
+        harness.begin_with_initial_hooks()
+
+        assert isinstance(harness.charm.model.unit.status, ActiveStatus)
