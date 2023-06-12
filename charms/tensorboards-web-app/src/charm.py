d40bb2c3baa9c0a612088cc499dc1764cf47c13d
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2023-06-12 18:32:12 +0300

    twa: Rewrite charm with sidecar pattern
    - Rewrite charm following the sidecar pattern and using Pebble to manage
      the workload. Jupyter-UI charm was used as an example for this rewrite.
    - Modify metadata.yaml to:
       - add a container
       - include information like website, source, docs, description
       - modify oci-image name
    - Import KubernetesServicePatch library using `charmcraft fetch-lib`.

--- charms/tensorboards-web-app/src/charm.py
+++ charms/tensorboards-web-app/src/charm.py
@@ -1,125 +1,197 @@
 #!/usr/bin/env python3
-# Copyright 2021 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import logging
+"""A Juju Charm for Tensorboards Web App."""
 
-from oci_image import OCIImageResource, OCIImageResourceError
+import logging
+from typing import Dict
+
+from charmed_kubeflow_chisme.exceptions import ErrorWithStatus, GenericCharmRuntimeError
+from charmed_kubeflow_chisme.kubernetes import KubernetesResourceHandler
+from charmed_kubeflow_chisme.lightkube.batch import delete_many
+from charmed_kubeflow_chisme.pebble import update_layer
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from lightkube import ApiError
+from lightkube.generic_resource import load_in_cluster_generic_resources
+from lightkube.models.core_v1 import ServicePort
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, Container, MaintenanceStatus, WaitingStatus
+from ops.pebble import Layer
 from serialized_data_interface import NoCompatibleVersions, NoVersionsListed, get_interfaces
 
-
-class CheckFailed(Exception):
-    """Raise this exception if one of the checks in main fails."""
-
-    def __init__(self, msg: str, status_type=None):
-        super().__init__()
-
-        self.msg = str(msg)
-        self.status_type = status_type
-        self.status = status_type(self.msg)
+K8S_RESOURCE_FILES = ["src/templates/auth_manifests.yaml.j2"]
+LIGHTKUBE_FIELD_MANAGER = "lightkube"
 
 
-class Operator(CharmBase):
+class TensorboardsWebApp(CharmBase):
     def __init__(self, *args):
+        """Initialize charm and setup the container."""
         super().__init__(*args)
 
-        self.log = logging.getLogger(__name__)
-        self.image = OCIImageResource(self, "oci-image")
+        self.logger = logging.getLogger(__name__)
+        self._namespace = self.model.name
+        self._name = self.app.name
+        self._http_port = self.model.config["port"]
 
-        for event in [
-            self.on.install,
-            self.on.upgrade_charm,
-            self.on.config_changed,
-            self.on.leader_elected,
-            self.on["ingress"].relation_changed,
-        ]:
-            self.framework.observe(event, self.main)
+        # Explicitly use defined string since the charm can be deployed with different app name
+        self._container_name = "tensorboards-web-app"
+        self._container = self.unit.get_container(self._container_name)
+        self._k8s_resource_handler = None
 
-    def main(self, event):
-        try:
-
-            self._check_leader()
-
-            interfaces = self._get_interfaces()
-
-            image_details = self._check_image_details()
-
-        except CheckFailed as check_failed:
-            self.model.unit.status = check_failed.status
-            return
-
-        self._configure_mesh(interfaces)
-
-        config = self.model.config
-
-        self.model.unit.status = MaintenanceStatus("Setting pod spec")
-
-        self.model.pod.set_spec(
-            {
-                "version": 3,
-                "serviceAccount": {
-                    "roles": [
-                        {
-                            "global": True,
-                            "rules": [
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["namespaces"],
-                                    "verbs": ["get", "list"],
-                                },
-                                {
-                                    "apiGroups": ["authorization.k8s.io"],
-                                    "resources": ["subjectaccessreviews"],
-                                    "verbs": ["create"],
-                                },
-                                {
-                                    "apiGroups": ["tensorboard.kubeflow.org"],
-                                    "resources": [
-                                        "tensorboards",
-                                        "tensorboards/finalizers",
-                                    ],
-                                    "verbs": ["get", "list", "create", "delete"],
-                                },
-                                {
-                                    "apiGroups": [""],
-                                    "resources": ["persistentvolumeclaims"],
-                                    "verbs": ["create", "delete", "get", "list"],
-                                },
-                                {
-                                    "apiGroups": ["storage.k8s.io"],
-                                    "resources": ["storageclasses"],
-                                    "verbs": ["get", "list", "watch"],
-                                },
-                                {
-                                    "apiGroups": ["kubeflow.org"],
-                                    "resources": ["poddefaults"],
-                                    "verbs": ["get", "list", "watch"],
-                                },
-                            ],
-                        }
-                    ]
-                },
-                "containers": [
-                    {
-                        "name": "tensorboards-web-app",
-                        "imageDetails": image_details,
-                        "envConfig": {
-                            "USERID_HEADER": "kubeflow-userid",
-                            "USERID_PREFIX": "",
-                            "APP_PREFIX": "/tensorboards",
-                            "APP_SECURE_COOKIES": str(config["secure-cookies"]),
-                            "BACKEND_MODE": config["backend-mode"],
-                        },
-                        "ports": [{"name": "http", "containerPort": config["port"]}],
-                    }
-                ],
-            },
+        http_port = ServicePort(int(self._http_port), name="http")
+        self.service_patcher = KubernetesServicePatch(
+            self, [http_port], service_name=f"{self._name}"
         )
 
-        self.model.unit.status = ActiveStatus()
+        # Set up event handlers
+        self.framework.observe(self.on.upgrade_charm, self._on_event)
+        self.framework.observe(self.on.config_changed, self._on_event)
+        self.framework.observe(self.on.leader_elected, self._on_event)
+        self.framework.observe(self.on["ingress"].relation_changed, self._on_event)
+        self.framework.observe(self.on.tensorboards_web_app_pebble_ready, self._on_event)
+
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.remove, self._on_remove)
+
+
+    @property
+    def k8s_resource_handler(self) -> KubernetesResourceHandler:
+        """Get the K8S resource handler."""
+        context = {
+            "app_name": self._name,
+            "namespace": self._namespace,
+            "service": self._name,
+        }
+
+        if not self._k8s_resource_handler:
+            self._k8s_resource_handler = KubernetesResourceHandler(
+                field_manager=LIGHTKUBE_FIELD_MANAGER,
+                template_files=K8S_RESOURCE_FILES,
+                context=context,
+                logger=self.logger,
+            )
+        load_in_cluster_generic_resources(self._k8s_resource_handler.lightkube_client)
+        return self._k8s_resource_handler
+
+    @k8s_resource_handler.setter
+    def k8s_resource_handler(self, handler: KubernetesResourceHandler):
+        """Set the K8S resource handler"""
+        self._k8s_resource_handler = handler
+
+    @property
+    def _env_vars(self) -> Dict[str, str]:
+        """Return environment variables based on model configuration."""
+        config = self.model.config
+        env_vars = {
+            "APP_PREFIX": "/tensorboards",
+            "APP_SECURE_COOKIES": str(config["secure-cookies"]),
+            "BACKEND_MODE": config["backend-mode"],
+            "USERID_HEADER": "kubeflow-userid",
+            "USERID_PREFIX": "",
+        }
+
+        return env_vars
+
+    @property
+    def _tensorboards_web_app_layer(self) -> Layer:
+        """Create and return Pebble framework layer."""
+        exec_command = (
+            "gunicorn"
+            " -w 3"
+            f" --bind 0.0.0.0:{self._http_port}"
+            " --access-logfile"
+            " - entrypoint:app"
+        )
+
+        layer_config = {
+            "summary": "jupyter-web-app layer",
+            "description": "Pebble config layer for jupyter-web-app",
+            "services": {
+                self._container_name: {
+                    "override": "replace",
+                    "summary": "Entrypoint of jupyter-web-app image",
+                    "command": exec_command,
+                    "startup": "enabled",
+                    "environment": self._env_vars,
+                    "on-check-failure": {"up": "restart"},
+                }
+            },
+            "checks": {
+                "up": {
+                    "override": "replace",
+                    "period": "30s",
+                    "http": {"url": f"http://localhost:{self._http_port}"},
+                },
+            },
+        }
+
+        return Layer(layer_config)
+
+    def _on_install(self, _) -> None:
+        """Perform installation only actions."""
+        try:
+            # Deploy K8S resources to speed up deployment
+            self._deploy_k8s_resources()
+        except ErrorWithStatus as err:
+            self._log_and_set_status(err.status)
+        return
+
+    def _on_remove(self, _) -> None:
+        """Remove all resources."""
+        self._log_and_set_status(MaintenanceStatus("Removing K8S resources"))
+        k8s_resources_manifests = self.k8s_resource_handler.render_manifests()
+        try:
+            delete_many(self.k8s_resource_handler.lightkube_client, k8s_resources_manifests)
+        except ApiError as err:
+            # Do not log/report when resources were not found
+            if err.status.code != 404:
+                self.logger.error(f"Removing K8S resources failed with error: {err}")
+                raise GenericCharmRuntimeError("Removing K8s resources failed") from err
+
+    def _on_event(self, event) -> None:
+        """Perform required actions for every event."""
+        try:
+            self._check_leader()
+            self._deploy_k8s_resources()
+            if self._is_container_ready():
+                update_layer(
+                    self._container_name,
+                    self._container,
+                    self._tensorboards_web_app_layer,
+                    self.logger,
+                )
+                interfaces = self._get_interfaces()
+                self._configure_mesh(interfaces)
+
+        except ErrorWithStatus as err:
+            self._log_and_set_status(err.status)
+            return
+
+        self.unit.status = ActiveStatus()
+
+    def _deploy_k8s_resources(self) -> None:
+        """Deploy K8S resources."""
+        try:
+            self._log_and_set_status(MaintenanceStatus("Creating K8S resources"))
+            self.k8s_resource_handler.apply()
+        except ApiError:
+            raise ErrorWithStatus("Creating K8S resources failed", BlockedStatus)
+        self._log_and_set_status(MaintenanceStatus("K8S resources created"))
+
+    def _is_container_ready(self) -> bool:
+        """Check if connection can be made with container.
+        Set maintenance status if container is not available.
+
+        Return:
+             False if container is not available
+             True if connection can be made
+        """
+        if not self._container.can_connect():
+            self._log_and_set_status(MaintenanceStatus("Waiting for pod startup to complete"))
+            return False
+        return True
 
     def _configure_mesh(self, interfaces) -> None:
         if interfaces["ingress"]:
@@ -133,20 +205,22 @@ class Operator(CharmBase):
             )
             self.unit.status = ActiveStatus()
         else:
-            raise CheckFailed("No ingress relation available", BlockedStatus)
+            raise ErrorWithStatus("No ingress relation available", BlockedStatus)
 
-    def _check_leader(self):
+    def _check_leader(self) -> None:
+        """Check if this unit is a leader."""
         if not self.unit.is_leader():
             # We can't do anything useful when not the leader, so do nothing.
-            raise CheckFailed("Waiting for leadership", WaitingStatus)
+            self.logger.info("Not a leader")
+            raise ErrorWithStatus("Waiting for leadership", WaitingStatus)
 
     def _get_interfaces(self):
         try:
             interfaces = get_interfaces(self)
         except NoVersionsListed as err:
-            raise CheckFailed(err, WaitingStatus)
+            raise ErrorWithStatus(err, WaitingStatus)
         except NoCompatibleVersions as err:
-            raise CheckFailed(err, BlockedStatus)
+            raise ErrorWithStatus(err, BlockedStatus)
         return interfaces
 
     def _log_and_set_status(self, status):
@@ -171,4 +245,4 @@ class Operator(CharmBase):
 
 
 if __name__ == "__main__":
-    main(Operator)
+    main(TensorboardsWebApp)
