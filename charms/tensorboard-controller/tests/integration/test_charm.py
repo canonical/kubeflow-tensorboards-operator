df35ab8ec8cd34e76ac9882fbb6a2fc232137e0e
Robert Gildein <robert.gildein@canonical.com>
2024-07-10 13:03:04 +0200

    bump chisme to 0.4.2

--- charms/tensorboard-controller/tests/integration/test_charm.py
+++ charms/tensorboard-controller/tests/integration/test_charm.py
@@ -7,11 +7,7 @@ from pathlib import Path
 
 import pytest
 import yaml
-from charmed_kubeflow_chisme.testing import (
-    GRAFANA_AGENT_APP,
-    assert_logging,
-    deploy_and_assert_grafana_agent,
-)
+from charmed_kubeflow_chisme.testing import assert_logging, deploy_and_assert_grafana_agent
 from lightkube import ApiError, Client, codecs
 from lightkube.generic_resource import (
     create_namespaced_resource,
@@ -94,7 +90,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
 async def test_logging(ops_test: OpsTest):
     """Test logging is defined in relation data bag."""
-    app = ops_test.model.applications[GRAFANA_AGENT_APP]
+    app = ops_test.model.applications[APP_NAME]
     await assert_logging(app)
 
 
