f2b6c0d93cfc79c6ff52598a0fd7e5024c8b0515
NohaIhab <noha.ihab@canonical.com>
2024-09-20 08:06:45 +0000

    feat: add terraform modules for tensorboard controller and web-app

---
+++ charms/tensorboard-controller/terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "app_name" {
+  value = juju_application.tensorboard_controller.name
+}
+
+output "provides" {
+  value = {
+    metrics_endpoint  = "metrics-endpoint",
+  }
+}
+
+output "requires" {
+  value = {
+    gateway_info = "gateway-info",
+    logging        = "logging"
+  }
+}
