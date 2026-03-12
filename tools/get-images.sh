dbef5322e12dfe51bb2cd648cd9de636374553ae
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2026-03-12 11:01:19 +0100

    Update get-images.sh (#238)

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -3,6 +3,10 @@
 # This script returns list of container images that are managed by this charm and/or its workload
 #
 # dynamic list
+
+set -xe
+
 IMAGE_LIST=()
 IMAGE_LIST+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST+=($(yq -N '.options.tensorboard-image.default' ./charms/tensorboard-controller/config.yaml))
 printf "%s\n" "${IMAGE_LIST[@]}"
